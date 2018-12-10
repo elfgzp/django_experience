1af8ab6839cc1e38c857e64a8cc3d13d0c536ecc
elf_gzp <741424975@qq.com>
2018-12-10 11:12:46 +0800

    [ADD] add models to development_of_test_habits app

--- development_of_test_habits/models.py
+++ development_of_test_habits/models.py
@@ -1,2 +1,22 @@
 from django.db import models
 
+
+class School(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Class(models.Model):
+    school_id = models.ForeignKey(to=School, on_delete=models.PROTECT)
+    name = models.CharField(max_length=32)
+
+
+class Student(models.Model):
+    class_id = models.ForeignKey(to=Class, on_delete=models.PROTECT)
+    name = models.CharField(max_length=32)
+
+
+class HomeWork(models.Model):
+    student_id = models.ForeignKey(to=Student, on_delete=Student)
+    name = models.CharField(max_length=32)
+
+
